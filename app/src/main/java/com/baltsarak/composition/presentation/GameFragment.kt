75f3f17f7d857c70e9205aababff7f8c73fe6ce7
Baltsarak <evgeniy.bk.89@gmail.com>
2023-11-29 14:36:34 +0300

    Transfer of parameters to the GameViewModel is organized using the GameViewModelFactory

--- app/src/main/java/com/baltsarak/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/baltsarak/composition/presentation/GameFragment.kt
@@ -20,7 +20,7 @@ class GameFragment : Fragment() {
     private val viewModel by lazy {
         ViewModelProvider(
             this,
-            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+            GameViewModelFactory(level, requireActivity().application)
         )[GameViewModel::class.java]
     }
 
@@ -56,7 +56,6 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeViewModel()
         setClickListenersToOptions()
-        viewModel.startGame(level)
     }
 
     private fun setClickListenersToOptions() {
